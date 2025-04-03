c19cc4355f734bb1783976f13d1a71824434b7dc
ashutosh <kurvheashutosh@gmail.com>
2025-04-03 22:54:16 +0530

    making changes

---
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -0,0 +1,117 @@
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
+import { Link } from "react-router-dom"
+
+const NewArrivals = () => {
+    const newArrivals = [
+      {
+        _id: "1",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=1",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+      {
+        _id: "2",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=2",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+      {
+        _id: "3",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=3",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+      {
+        _id: "4",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=4",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+      {
+        _id: "5",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=5",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+      {
+        _id: "6",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=6",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+      {
+        _id: "7",
+        name: "Stylish Jacket",
+        price: 120,
+        images: [
+          {
+            url: "https://picsum.photos/500/500?random=7",
+            altText: "Stylish-Jacket",
+          },
+        ],
+      },
+    ];
+  return (
+      <section className='container mx-auto text-center mb-10 relative'>
+          <h2 className='text-3xl font-bold mb-4 '>Explore New Arrivals</h2>
+          <p className='text-lg text-gray-600 m-80'>
+              Discover the latest styles straight off the runway  freshly added to keep your wardrobe on the cutting edge of fashion.
+          </p>
+
+          {/* Scroll Button */}
+          <div className='absolute right-0 bottom-[-30x] flex space-x-2'>
+              <button className='p-2 rounded border bg-white text-black'>
+                  <FiChevronLeft className="text-2xl "/>
+              </button>
+              <button className='p-2 rounded border bg-white text-black'>
+                  <FiChevronRight className="text-2xl "/>
+              </button>
+          </div>
+          {/* Scrollable items */}
+          <div className="container mx-auto overflow-x-scroll flex space-x-6 relative">
+              {newArrivals.map((product) => {
+                  <div key={product._id} className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative">
+                      <img src={product.images[0]?.url} alt={product.images[0]?.altText || product.name} />
+                      <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
+                          <Link to={`/product/${product._id}`} className="block" />
+                          <h4 className="font-medium">{product.name}</h4>
+                          <p className="mt-1">${product.price}</p>
+                      </div>
+                  </div>
+              })}
+          </div> 
+    </section>
+  )
+}
+
+export default NewArrivals
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import Hero from "../components/Layout/Hero"
+import GenderCollectionSection from "../components/Products/GenderCollectionSection"
+import NewArrivals from "../components/Products/NewArrivals"
 const Home = () => {
   return (
     <div>
-      <Hero />
+          <Hero />
+          <GenderCollectionSection />
+          <NewArrivals />
     </div>
   )
 }
