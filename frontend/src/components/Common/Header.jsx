72ece3f861afbd4febbd1e5903dd074cde69fcd0
ashutosh <kurvheashutosh@gmail.com>
2025-04-03 14:12:41 +0530

    adding some new files

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,10 +1,12 @@
 import Topbar from "../Layout/Topbar"
+import Navbar from "./Navbar"
 const Header = () => {
   return (
       <>
           {/* Topbar */}
           <Topbar/>
           {/* Navbar */}
+          <Navbar/>
           {/* Cart Drawer */}
       </>
   )
