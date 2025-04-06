d36ff51928673ab04bbd4ba4fb5dc0a62aaf5dda
ashutosh <kurvheashutosh@gmail.com>
2025-04-06 20:00:55 +0530

    Frontend Completed

--- frontend/src/components/Admin/AdminSidebar.jsx
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -58,7 +58,7 @@ const AdminSidebar = () => {
         </NavLink>
 
         <NavLink
-          to="/admin/shop"
+          to="/"
           className={({ isActive }) =>
             isActive
               ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
