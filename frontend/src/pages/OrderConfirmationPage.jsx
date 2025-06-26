f5f3992d8f9e1985ad137088d487907806c32593
ashutosh <kurvheashutosh@gmail.com>
2025-06-26 21:31:17 +0530

    Making some changes

--- frontend/src/pages/OrderConfirmationPage.jsx
+++ frontend/src/pages/OrderConfirmationPage.jsx
@@ -1,33 +1,3 @@
-// const checkout = {
-//   _id: "12334",
-//   createdAt: new Date(),
-//   checkoutItems: [
-//     {
-//       productId: "1",
-//       name: "Jacket",
-//       color: "black",
-//       size: "M",
-//       price: 150,
-//       quantity: 1,
-//       image: "https://picsum.photos/150?random=1",
-//     },
-//     {
-//       productId: "2",
-//       name: "Jeans",
-//       color: "black",
-//       size: "M",
-//       price: 750,
-//       quantity: 1,
-//       image: "https://picsum.photos/150?random=2",
-//     },
-//   ],
-//   shippingAddress: {
-//     address: "123 Fashion Street",
-//     city: "New York",
-//     country: "USA",
-//   },
-// };
-
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
